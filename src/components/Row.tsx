50ea073f0d1d7d11f0d1b57398b3a1187d99df87
Nils Backe <nils.backe@gmail.com>
2022-02-09 23:01:28 -0500

    new coloring

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Bet, Game, PlayerGameStats } from '../types'
 
-const Container = styled.div<{ color: string }>`
-	background-color: ${({ color }) => color};
+const Container = styled.div`
 	margin: 16px 0px;
 	border-radius: 24px;
 	padding: 24px;
@@ -11,12 +10,24 @@ const Container = styled.div<{ color: string }>`
 	flex-direction: row;
 	align-items: center;
 	justify-content: space-between;
+	background-color: transparent;
+	position: relative;
 `
 
 const Column = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+`
+
+const HalfBackground = styled.div<{ color: string; right?: boolean }>`
+	background-color: ${({ color }) => color};
+	position: absolute;
+	z-index: -1;
+	top: 0px;
+	bottom: 0px;
+	${({ right }) => (right ? `right: 0px;` : `left: 0px;`)}
+	width: 50%;
 `
 
 interface Props {
@@ -29,27 +40,47 @@ export const Row: React.FC<Props> = ({ bet, stats, game }) => {
 	const team = game && (bet.teamId === game.home_team.id ? game.home_team.name : game.visitor_team.name)
 
 	let pointsOnTrack = false
+	let pointsMade = false
 	if (game && game.period > 0 && stats && bet.pointsGoal && stats.pts / Number(bet.pointsGoal) > game.period / 4) {
-		pointsOnTrack = true
+		if (stats.pts >= Number(bet.pointsGoal)) {
+			pointsMade = true
+		} else {
+			pointsOnTrack = true
+		}
 	}
 	let threesOnTrack = false
+	let threesMade = false
 	if (game && game.period > 0 && stats && bet.threesGoal && stats.fg3m / Number(bet.threesGoal) > game.period / 4) {
-		threesOnTrack = true
+		if (stats.fg3m >= Number(bet.threesGoal)) {
+			threesMade = true
+		} else {
+			threesOnTrack = true
+		}
 	}
 
-	let color = 'beige'
+	let leftColor = 'beige'
+	let rightColor = 'beige'
 	if (game?.status === 'Final') {
-		color = 'transparent'
+		leftColor = rightColor = 'transparent'
 	} else if (game?.status.endsWith('ET')) {
-		color = 'lightgrey'
-	} else if (pointsOnTrack && threesOnTrack) {
-		color = 'lightgreen'
-	} else if (pointsOnTrack || threesOnTrack) {
-		color = 'yellow'
+		leftColor = rightColor = 'lightgrey'
+	} else {
+		if (pointsOnTrack) {
+			leftColor = 'yellow'
+		}
+		if (pointsMade) {
+			leftColor = 'green'
+		}
+		if (threesOnTrack) {
+			rightColor = 'yellow'
+		}
+		if (threesMade) {
+			rightColor = 'green'
+		}
 	}
 
 	return (
-		<Container color={color}>
+		<Container>
 			<span>{bet.toWin}</span>
 			<span>
 				{game?.status} {game?.time && `-`} {game?.time}
@@ -73,6 +104,8 @@ export const Row: React.FC<Props> = ({ bet, stats, game }) => {
 				<span>Current threes: {stats?.fg3m}</span>
 				<span>Threes needed: {bet.threesGoal}</span>
 			</Column>
+			<HalfBackground color={leftColor} />
+			<HalfBackground color={rightColor} right />
 		</Container>
 	)
 }
